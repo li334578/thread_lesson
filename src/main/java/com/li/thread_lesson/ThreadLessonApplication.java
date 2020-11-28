e61c5b5f4efe9f0534e4dd53638cf57a4d8a9c7e
LiWenBo <liwenbo4922@163.com>
2020-11-28 10:09:41 +0800

    Initial commit

---
+++ src/main/java/com/li/thread_lesson/ThreadLessonApplication.java
@@ -0,0 +1,13 @@
+package com.li.thread_lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadLessonApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/li/thread_lesson/thread_demo/Test01.java
@@ -0,0 +1,10 @@
+package com.li.thread_lesson.thread_demo;
+
+/**
+ * @author : LiWenBo
+ * @program : thread_lesson
+ * @description :
+ * @date : 2020-11-28 10:08:31
+ */
+public class Test01 {
+}
